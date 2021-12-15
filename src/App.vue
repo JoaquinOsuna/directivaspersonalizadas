ef7e527ad322e84878dda945e423cc15ef128c86
Joaquin <joaquin.zatarain@cceo.com.mx>
2021-12-15 14:52:49 -0600

    Iniciamos

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+      <div class="jumboton">
+        <span v-decorar>Aprende Vue.js 2 Facil</span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  
+
+}
+</script>
+
+<style>
+
+</style>
